47d92f28f13a204925c67a1df1fede8c306451d4
ThisizSid <bhattasiddhant950@gmail.com>
2025-03-10 10:38:49 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+from langchain_huggingface import HuggingFaceEmbeddings
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate respone based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+
+    """
+
+)
+
+def create_vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        st.session_state.loader=PyPDFDirectoryLoader("research_papers")
+    #     uploaded_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
+    # ## Process uploaded  PDF's
+    #     if uploaded_files:
+    #         documents=[]
+    #         for uploaded_file in uploaded_files:
+    #             temppdf=f"./temp.pdf"
+    #             with open(temppdf,"wb") as file:
+    #                 file.write(uploaded_file.getvalue())
+    #                 file_name=uploaded_file.name
+
+    #             loader=PyPDFLoader(temppdf)
+    #             docs=loader.load()
+    #             documents.extend(docs)
+# Combine all documents 
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+st.title("PDF Q&A")
+
+user_prompt=st.text_input("Enter your query from the research paper")
+
+if st.button("CLICK HERE BEFORE QUERYING!"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+import time
+
+if user_prompt:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':user_prompt})
+    print(f"Response time :{time.process_time()-start}")
+
+    st.write(response['answer'])
+
+
+
+
+
+
+
